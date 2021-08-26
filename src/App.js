2f23a1cddfe00e43636c4e5df19321935c606c45
ali <ali.mehry@outlook.com>
2021-08-26 16:31:11 +0430

    commit all

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItems';
+
+function App() {
+  return (
+    <div>
+      <h2>let's get started</h2>
+      <p>this is also visalbe</p>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,20 @@
+import "./ExpenseItems.css";
+// import React from "react";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 8, 25);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
